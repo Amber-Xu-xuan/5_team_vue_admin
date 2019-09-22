52bd3812c2c6c29c2b019699b07e4ecf31d980ca
Amber-Xu-xuan <m15860493120@163.com>
2019-09-22 21:32:54 +0800

    使用echart统计订单数量

--- orderFood_vue_Admin/src/views/charts/orderByDay.vue
+++ orderFood_vue_Admin/src/views/charts/orderByDay.vue
@@ -14,6 +14,9 @@
 
 		<!--列表-->
 		<template>
+
+			<div :class="className" :id="id" :style="{height:height,width:width}" ref="statisticOrderEchart">
+			</div>
 			<el-table :data="users" highlight-current-row v-loading="loading" style="width: 100%;">
 				<el-table-column type="index" width="60">
 				</el-table-column>
@@ -30,9 +33,29 @@
 	</section>
 </template>
 <script>
+// <!--引入echart-->
+import echarts from 'echarts'
 	import { selectOrdeByDay } from '../../api/api';
 	//import NProgress from 'nprogress'
 	export default {
+        props: {
+            className: {
+                type: String,
+                default: 'yourClassName'
+            },
+            id: {
+                type: String,
+                default: 'staticOrder'
+            },
+            width: {
+                type: String,
+                default: '800px'
+            },
+            height: {
+                type: String,
+                default: '400px'
+            }
+        },
 		data() {
 			return {
 				filters: {
@@ -40,9 +63,20 @@
 				},
 				loading: false,
 				users: [
-				]
+				],
+                orderCountList: [],
+                priceTotalList: [],
+				timeList: [],
+                chart: null,
 			}
 		},
+        beforeDestroy() {
+            if (!this.chart) {
+                return
+            }
+            this.chart.dispose();
+            this.chart = null;
+        },
 		methods: {
 			//支付状态转换
 			formatPay: function (row, column) {
@@ -51,6 +85,117 @@
             //订单状态转换
             formatOrder: function (row, column) {
                 return row.status == 1 ? '配送中' : row.payStatus == 0 ? '已取消' : '配送完成';
+            },
+            initChart() {
+                this.chart = echarts.init(this.$refs.statisticOrderEchart);
+                // 从后端请求数据
+                        this.$message.success("统计数据如下");
+                        this.chart.setOption({
+                            color: ['#3398DB'],
+                            title: {
+                                text: '订餐数据分析'
+                            },
+                            //提示框组件
+                            tooltip: {
+                                trigger: 'axis', // 坐标轴触发，主要在柱状图，折线图等会使用类目轴的图表中使用。，item
+                                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                                }
+                            },
+                            grid: {
+                                left: '3%',
+                                right: '4%',
+                                bottom: '3%',
+                                containLabel: true
+                            },
+                            xAxis: [{
+                                type: 'category',
+                                data: this.timeList,
+                                axisTick: {
+                                    alignWithLabel: true
+                                }
+                            }],
+                            yAxis: [{
+                                type: 'value',
+                                boundaryGap: false,
+                            }],
+                            //折线图上方的工具箱
+                            toolbox: {
+                                left: 'center',
+                                feature: {
+                                    dataZoom: {
+                                        yAxisIndex: 'none'
+                                    },
+                                    restore: {},
+                                    saveAsImage: {}
+                                }
+                            },
+                            //区域缩放
+                            dataZoom:[{
+                                //绝对数值形式
+                                startValue: '2014-06-01'
+                            }, {
+                                type: 'inside'
+                            }],
+                            series: [{
+                                name: '订餐数量',
+                                //bar：柱状图 line为折线图
+                                type: 'line',
+                                //区域颜色
+                                areaStyle: {},
+                                smooth: true,
+                                data: this.orderCountList,
+                                markLine: {
+                                    silent: true,
+                                    data: [{
+                                        yAxis: 10
+                                    }, {
+                                        yAxis: 20
+                                    }, {
+                                        yAxis: 50
+                                    }, {
+                                        yAxis: 100
+                                    }, {
+                                        yAxis: 200
+                                    }]
+                                }
+                            }],
+                            //  折线图的颜色
+                            visualMap: {
+                                top: 10,
+                                right: 10,
+                                pieces: [{
+                                    gt: 0,
+                                    lte: 5,
+                                    color: '#096'
+                                }, {
+                                    gt: 5,
+                                    lte: 10,
+                                    color: '#ffde33'
+                                }, {
+                                    gt: 10,
+                                    lte: 20,
+                                    color: '#ff9933'
+                                }, {
+                                    gt: 20,
+                                    lte: 30,
+                                    color: '#cc0033'
+                                }, {
+                                    gt: 30,
+                                    lte: 40,
+                                    color: '#660099'
+                                }, {
+                                    gt: 40,
+                                    color: '#7e0023'
+                                }],
+                                outOfRange: {
+                                    color: '#999'
+                                }
+                            },
+                        })
+                //前端测试
+                // 把配置和数据放这里
+
             },
 			//获取商品相关的 订单明细列表
 			getUser: function () {
@@ -63,6 +208,12 @@
                 selectOrdeByDay().then((res) => {
                     if(res.data.status="0"){
                         this.users = res.data.data;
+                        for (var user of res.data.data){
+                            this.timeList.push(user.time);
+							this.orderCountList.push(user.orderCount);
+							this.priceTotalList.push(user.priceTotal);
+						}
+                        this.initChart();
                         this.loading = false;
 					}else{
                         this.loading = true;
